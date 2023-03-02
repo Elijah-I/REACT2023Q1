1c3ac60d6cfc3d9c9ba5b8447cf6adb5f7784ac6
Elijah <elijah.ivanik@inbox.ru>
2023-03-02 09:31:30 +0300

    fix: Vite base path config

--- react.components/vite.config.js
+++ react.components/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'elijah-i-REACT2023Q1/react.components',
+  base: '/elijah-i-REACT2023Q1/react.components',
   plugins: [react(), tsconfigPaths()],
 });

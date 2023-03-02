656c4ebba7227c43ee043d04fb8d23a42b8bb4c3
Elijah <elijah.ivanik@inbox.ru>
2023-03-02 17:01:16 +0300

    feat: implement 404 page

--- react.components/src/components/error/index.tsx
+++ react.components/src/components/error/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import ROUTES from 'types/routes.types';
+import imgSrc from '../../assets/img/404.jpg';
+import './index.scss';
 
 class Error extends React.Component {
   render() {
-    return <>404</>;
+    return (
+      <div className="error__wrapper">
+        <div className="container error__container">
+          <NavLink to={ROUTES.ROOT} className="error__block">
+            <img src={imgSrc} alt="not found" />
+          </NavLink>
+        </div>
+      </div>
+    );
   }
 }
 

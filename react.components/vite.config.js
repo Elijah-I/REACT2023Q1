53fa47c54e6da7f072b3fa71aa45e608e1086d20
Elijah <elijah.ivanik@inbox.ru>
2023-03-02 15:23:58 +0300

    feat: implement Vite-eslint support

--- react.components/vite.config.js
+++ react.components/vite.config.js
@@ -1,8 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/elijah-i-REACT2023Q1/react.components',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), eslint()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });

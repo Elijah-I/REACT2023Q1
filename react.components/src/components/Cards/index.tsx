d283849812a9b917fb0bb65bdd4a5ed2325278a4
Elijah <elijah.ivanik@inbox.ru>
2023-03-05 12:16:49 +0300

    feat: apply Search to Cards

--- react.components/src/components/Cards/index.tsx
+++ react.components/src/components/Cards/index.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import Card from './Card';
-import type { CardsProps } from 'types/card.types';
+import type { Card as ICard, CardsProps } from 'types/card.types';
 import './index.scss';
 
 class Cards extends React.PureComponent<CardsProps> {
+  constructor(props: CardsProps) {
+    super(props);
+
+    this.filter = this.filter.bind(this);
+  }
+
+  filter(card: ICard) {
+    let pass = true;
+    const filter = this.props.search;
+
+    const containedIn = (str: string) => {
+      return str.toLowerCase().includes(filter.search.toLowerCase());
+    };
+
+    if (filter.option !== 'all') {
+      if (filter.option !== card.type) pass = false;
+    }
+
+    if (pass && filter.search) {
+      pass = false;
+
+      if (containedIn(card.author)) pass = true;
+      if (containedIn(card.title)) pass = true;
+      if (card.tags.some(containedIn)) pass = true;
+    }
+
+    return pass;
+  }
+
   render() {
-    if (!this.props.cards.length) {
+    const filteredCards = this.props.cards.filter(this.filter);
+
+    if (!filteredCards.length) {
       return <div className="cards__null">no cards found</div>;
     }
 
     return (
       <div className="cards__list">
-        {this.props.cards.map((card) => (
+        {filteredCards.map((card) => (
           <Card key={card.id} info={card} />
         ))}
       </div>

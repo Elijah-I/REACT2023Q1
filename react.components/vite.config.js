64cd145cc508dd972e50331bd313e00a143f8341
Elijah <elijah.ivanik@inbox.ru>
2023-03-21 12:12:42 +0300

    fix: deploy path

--- react.components/vite.config.js
+++ react.components/vite.config.js
@@ -1,11 +1,11 @@
 import { resolve } from 'path';
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/REACT2023Q1/react.components',
+  base: '/REACT2023Q1/react.forms',
   plugins: [react(), tsconfigPaths(), eslint()],
   test: {
     globals: true,

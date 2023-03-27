894cfc21d07de896f1544ad17d700f57ca50b98e
Elijah <elijah.ivanik@inbox.ru>
2023-03-27 10:57:18 +0300

    refactor: transit Root to FC

--- react.components/src/components/root/index.tsx
+++ react.components/src/components/root/index.tsx
@@ -3,17 +3,15 @@ import { Outlet } from 'react-router-dom';
 
 import Header from 'components/Header';
 
-class Root extends React.PureComponent {
-  render() {
-    return (
-      <>
-        <Header />
-        <main className="container">
-          <Outlet />
-        </main>
-      </>
-    );
-  }
-}
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 export default Root;

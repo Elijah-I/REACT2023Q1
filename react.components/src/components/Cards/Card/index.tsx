3399ca82b68235a21c8ef38a16c5fee19b8d4652
Elijah <elijah.ivanik@inbox.ru>
2023-03-27 11:00:28 +0300

    refactor: transit Cards to FC

--- react.components/src/components/Cards/Card/index.tsx
+++ react.components/src/components/Cards/Card/index.tsx
@@ -4,48 +4,46 @@ import viewsImgSrc from '../../../assets/icons/views.svg';
 import likesImgSrc from '../../../assets/icons/likes.svg';
 import './index.scss';
 
-class Card extends React.PureComponent<CardProps> {
-  render() {
-    return (
-      <div className="card">
-        <div className="card__img">
-          <img src={this.props.info.picture} alt="preview" />
-        </div>
+const Card = ({ info }: CardProps) => {
+  return (
+    <div className="card">
+      <div className="card__img">
+        <img src={info.picture} alt="preview" />
+      </div>
 
-        <div className="card__content">
-          <div className="card__title">{this.props.info.title}</div>
-          <div className="card__author">
-            by:
-            <b>
-              {this.props.info.author}
-              <i>[{this.props.info.date}]</i>
-            </b>
-          </div>
-          <div className="card__tags">
-            {this.props.info.tags.map((tag, key) => (
-              <b key={key}>{tag}</b>
-            ))}
-          </div>
+      <div className="card__content">
+        <div className="card__title">{info.title}</div>
+        <div className="card__author">
+          by:
+          <b>
+            {info.author}
+            <i>[{info.date}]</i>
+          </b>
         </div>
-
-        <div className="card__pannel">
-          <div className="card__info">
-            <div className="card__likes">
-              <img src={likesImgSrc} alt="like icon" />
-              <i>{this.props.info.statistic.likes}</i>
-            </div>
-            <div className="card__views">
-              <img src={viewsImgSrc} alt="view icon" />
-              <i>{this.props.info.statistic.views}</i>
-            </div>
-          </div>
-          <div className="card__favorite">
-            <b className={this.props.info.statistic.isFavorite ? 'card__favorite-active' : ''}></b>
-          </div>
+        <div className="card__tags">
+          {info.tags.map((tag, key) => (
+            <b key={key}>{tag}</b>
+          ))}
         </div>
       </div>
-    );
-  }
-}
+
+      <div className="card__pannel">
+        <div className="card__info">
+          <div className="card__likes">
+            <img src={likesImgSrc} alt="like icon" />
+            <i>{info.statistic.likes}</i>
+          </div>
+          <div className="card__views">
+            <img src={viewsImgSrc} alt="view icon" />
+            <i>{info.statistic.views}</i>
+          </div>
+        </div>
+        <div className="card__favorite">
+          <b className={info.statistic.isFavorite ? 'card__favorite-active' : ''}></b>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- react.components/src/components/Cards/index.tsx
+++ react.components/src/components/Cards/index.tsx
@@ -3,16 +3,10 @@ import Card from './Card';
 import type { Card as ICard, CardsProps } from 'types/card.types';
 import './index.scss';
 
-class Cards extends React.PureComponent<CardsProps> {
-  constructor(props: CardsProps) {
-    super(props);
-
-    this.filter = this.filter.bind(this);
-  }
-
-  filter(card: ICard) {
+const Cards = ({ cards, search }: CardsProps) => {
+  const filter = (card: ICard) => {
     let pass = true;
-    const filter = this.props.search;
+    const filter = search;
 
     if (!filter) return pass;
 
@@ -33,23 +27,21 @@ class Cards extends React.PureComponent<CardsProps> {
     }
 
     return pass;
+  };
+
+  const filteredCards = cards.filter(filter);
+
+  if (!filteredCards.length) {
+    return <div className="cards__null">no cards found</div>;
   }
 
-  render() {
-    const filteredCards = this.props.cards.filter(this.filter);
-
-    if (!filteredCards.length) {
-      return <div className="cards__null">no cards found</div>;
-    }
-
-    return (
-      <div className="cards__list">
-        {filteredCards.map((card) => (
-          <Card key={card.id} info={card} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cards__list">
+      {filteredCards.map((card) => (
+        <Card key={card.id} info={card} />
+      ))}
+    </div>
+  );
+};
 
 export default Cards;

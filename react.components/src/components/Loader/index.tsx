cba66536dc3684e8a02027086fe944f091d158e1
Elijah <elijah.ivanik@inbox.ru>
2023-03-27 10:35:59 +0300

    refactor: transit Loader to FC

--- react.components/src/components/Loader/index.tsx
+++ react.components/src/components/Loader/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './index.scss';
 
-class Loader extends React.PureComponent {
-  render() {
-    return <div className="loader"></div>;
-  }
-}
+const Loader = () => <div className="loader"></div>;
 
 export default Loader;

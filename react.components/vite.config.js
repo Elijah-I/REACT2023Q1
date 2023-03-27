9e5b2f862b87b860086d1dfc0b3d74e3cc6747a4
Elijah <elijah.ivanik@inbox.ru>
2023-03-27 22:06:09 +0300

    refactor: change deploy path to Hooks

--- react.components/vite.config.js
+++ react.components/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/REACT2023Q1/react.forms',
+  base: '/REACT2023Q1/react.hooks',
   plugins: [react(), tsconfigPaths(), eslint()],
   test: {
     globals: true,

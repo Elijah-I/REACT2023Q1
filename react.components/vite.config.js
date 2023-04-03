baea97940d6fab0ce162c28a479f030132bd5403
Elijah <elijah.ivanik@inbox.ru>
2023-04-03 09:49:24 +0300

    refactor: configure deploy to a new project

--- react.components/vite.config.js
+++ react.components/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/REACT2023Q1/react.hooks',
+  base: '/REACT2023Q1/React.API',
   plugins: [react(), tsconfigPaths(), eslint()],
   test: {
     globals: true,

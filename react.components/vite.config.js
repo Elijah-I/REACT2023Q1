9fbddf58a2d19ae6cb621e97655dc23ee43b98ff
Elijah <elijah.ivanik@inbox.ru>
2023-04-11 12:58:28 +0300

    fix: deploy

--- react.components/vite.config.js
+++ react.components/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/REACT2023Q1/React.API',
+  base: '/REACT2023Q1/React.Redux',
   plugins: [react(), tsconfigPaths(), eslint()],
   test: {
     globals: true,
